2b66c8e8b425dc80a93d2adacb139c43c4cd3baf
Peter Garamvolgyi <th307q@gmail.com>
2016-07-16 20:01:39 -0700

    add deployment config

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ var restify = require('restify');
 var deepcopy = require("deepcopy");
 
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+server.listen(process.env.PORT, function () {
+   console.log('%s listening to %s', server.name, server.url);
 });
-  
+
 var connector = new builder.ChatConnector({
-    appId: '',
-    appPassword: ''
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
 var bot = new builder.UniversalBot(connector);
